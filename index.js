ea67792848956efc296eb0f2508d5fc6bcc6fbff
Guilerme Pereira da Rocha <guilhermepereira0720@gmail.com>
2023-11-26 00:03:30 -0300

    1º primeira execução do servidor

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const express = require("express")
+const app = express()
+
+app.get('/', (requisicao, resposta) =>{
+    resposta.send("Oá mundo")
+})
+
+app.listen(3000, () =>{
+    console.log("Servidor rodando na porta 3000!")
+})
