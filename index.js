e17768dd379bb952cc758c69426598e72b8a01f1
Guilerme Pereira da Rocha <guilhermepereira0720@gmail.com>
2023-11-28 09:16:30 -0300

    exibição das tarefas na pág home finalizada

--- index.js
+++ index.js
@@ -53,12 +53,10 @@ app.get('/', (requisicao, resposta) =>{
             }
         })
 
-        
+        resposta.render('home', {tarefas})
 
     })
 
-
-    resposta.render('home')
 })
 
 const conexao = mysql.createConnection({
